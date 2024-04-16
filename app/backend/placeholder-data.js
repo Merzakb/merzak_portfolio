ca90131d27f35dbc798327c9096fb0108391cd62
Merzak Benaissi <merzak89merzak@gmail.com>
2024-04-16 14:41:54 +0200

    add projects data & images

---
+++ app/backend/placeholder-data.js
@@ -0,0 +1,73 @@
+
+const users = [
+    {
+      id: '410544b2-4001-4271-9855-fec4b6a6442a',
+      name: 'User',
+      email: 'user@nextmail.com',
+      password: '123456',
+    },
+];
+  
+const projects = [
+    {
+      id: '1',
+      name: 'Booki',
+      title: "Créez la page d'accueil d'une agence de voyage avec HTML & CSS",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+      image_url: '/images/projects/booki.png',
+      github_url: "https://github.com/Merzakb/P3_Booki_Benaissi_Merzak",
+      demo_url: "https://merzakb.github.io/P3_Booki_Benaissi_Merzak/"
+    },
+    {
+        id: '2',
+        name: 'Ohmyfood',
+        title: "Améliorez l'interface d'un site mobile avec des animations CSS",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+        image_url: '/images/projects/ohmyfood.png',
+        github_url: "https://github.com/Merzakb/P4_Ohmyfood_benaissi_merzak",
+        demo_url: ""
+    },
+    {
+        id: '3',
+        name: 'Sophie Bluel',
+        title: "Créez une page web dynamique avec JavaScript",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+        image_url: '/images/projects/sophiebluel.png',
+        github_url: "https://github.com/Merzakb/P6-sophie-bluel",
+        demo_url: ""
+    },
+    {
+        id: '4',
+        name: 'Kasa',
+        title: "Créez une application web de location immobilière avec React",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+        image_url: '/images/projects/kasa.png',
+        github_url: "https://github.com/Merzakb/P8_Kasa_benaissi_merzak",
+        demo_url: ""
+    },
+    {
+        id: '5', 
+        name: 'Nina Carducci',
+        title: "Optimisez le référencement d'un site de photographe",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+        image_url: '/images/projects/ninacarducci.png',
+        github_url: "https://github.com/Merzakb/P9_NinaCarducci_benaissi_merzak",
+        demo_url: ""
+    },
+    {
+        id: '6',
+        name: 'Kasa',
+        title: "Implémentez le front-end d'une application bancaire avec React",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis odit sequi quaerat quam deleniti incidunt, ipsum sapiente corrupti quisquam architecto nesciunt exercitationem excepturi! Aliquam deleniti doloremque debitis culpa, optio voluptatibus",
+        image_url: '/images/projects/argentbank.png',
+        github_url: "https://github.com/Merzakb/P11_benaissi-merzak_ArgentBank-website",
+        demo_url: ""
+    },
+];
+  
+  
+module.exports = {
+    users,
+    projects
+};
+  
