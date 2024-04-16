c50fd70e835fdce61cabc253f69e2ffc30a317e4
Merzak Benaissi <merzak89merzak@gmail.com>
2024-04-16 11:34:33 +0200

    add some fonts from google fonts

---
+++ app/ui/assets/fonts/fonts.js
@@ -0,0 +1,18 @@
+import { Inter, Roboto, Syne, Montserrat } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const  roboto = Roboto({
+  weight: ['300', '700'],
+  subsets: ['latin'],
+});
+
+export const  syne = Syne({
+    weight: ['400', '600','700'],
+    subsets: ['latin'],
+});
+export const  montserrat = Montserrat({
+    weight: ['300', "400", "500",'600','700'],
+    subsets: ['latin'],
+});
+
