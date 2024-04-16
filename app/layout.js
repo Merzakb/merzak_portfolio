875d92d10fbe0cec560ecac38367ae64be5d0e35
Merzak Benaissi <merzak89merzak@gmail.com>
2024-04-16 14:57:21 +0200

    edit layout component

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
-
 import { montserrat, syne } from './ui/assets/fonts/fonts';
+import Head from 'next/head';
 import './ui/styles/scss/globals.scss'
 import "./globals.css";
 import Header from './ui/components/header/Header';
 import Footer from './ui/components/footer/Footer';
-
-
 
 export const metadata = {
   title: "Merzak Benaissi | Portfolio",
@@ -18,16 +16,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+        <Head>
+            <script
+              src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+              integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+              crossOrigin="anonymous"
+            />
+        </Head>
         <body className={` ${montserrat.className}`}>
             <Header />
             <main>
                 {children}
             </main>
             <Footer />
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                crossOrigin="anonymous">
-            </script>
         </body>
     </html>
   );
